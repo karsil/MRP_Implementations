45b97045e8d2b5d13786963d154842659bf585f7
Jan Steeg <jan.n.steeg@student.fh-kiel.de>
2020-07-15 14:45:53 +0200

    fix path to server version

--- FasterRCNN/train.sh
+++ FasterRCNN/train.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-PIPELINE_CONFIG_PATH=./training/pipeline.config
-MODEL_DIR=./training
-NUM_TRAIN_STEPS=10
+PIPELINE_CONFIG_PATH=./model/pipeline.config
+MODEL_DIR=./model
+NUM_TRAIN_STEPS=20
 SAMPLE_1_OF_N_EVAL_EXAMPLES=1
-python3 ../../MASTERPROJECT/models/research/object_detection/model_main.py \
+python3 ../../models/research/object_detection/model_main.py \
     --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
     --model_dir=${MODEL_DIR} \
     --num_train_steps=${NUM_TRAIN_STEPS} \
