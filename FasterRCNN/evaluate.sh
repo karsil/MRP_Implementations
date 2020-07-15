2657ad83bc6b026258f44ba62b5315e675fd533a
jsteeg <jan.n.steeg@student.fh-kiel.de>
2020-07-15 16:06:16 +0000

    using legacy evaluation

--- FasterRCNN/evaluate.sh
+++ FasterRCNN/evaluate.sh
@@ -3,9 +3,10 @@ PIPELINE_CONFIG_PATH=./model/faster_rcnn_inception_v2_ufo.config
 CHECKPOINT_DIR=inference_graph/
 MODEL_DIR=eval/
 
-python3 ~/models/research/object_detection/model_main.py \
+# legacy
+python3 ~/models/research/object_detection/legacy/eval.py \
     --logtostderr \
-    --model_dir=${MODEL_DIR} \
     --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
     --checkpoint_dir=${CHECKPOINT_DIR} \
-    --run_once=true
+    --eval_dir=${EVAL_DIR} \
+    --eval_config_path=${EVAL_CONFIG} \
