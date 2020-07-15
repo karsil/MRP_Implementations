8b77551474bad28e8a1f56658aab919c12a75fdf
jsteeg <jan.n.steeg@student.fh-kiel.de>
2020-07-15 16:06:46 +0000

    changed back to current version of evaluation

--- FasterRCNN/evaluate.sh
+++ FasterRCNN/evaluate.sh
@@ -3,10 +3,9 @@ PIPELINE_CONFIG_PATH=./model/faster_rcnn_inception_v2_ufo.config
 CHECKPOINT_DIR=inference_graph/
 MODEL_DIR=eval/
 
-# legacy
-python3 ~/models/research/object_detection/legacy/eval.py \
+python3 ~/models/research/object_detection/model_main.py \
     --logtostderr \
+    --model_dir=${EVAL_DIR} \
     --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
-    --checkpoint_dir=${CHECKPOINT_DIR} \
-    --eval_dir=${EVAL_DIR} \
-    --eval_config_path=${EVAL_CONFIG} \
+    --checkpoint_dir=${MODEL_DIR} \
+    --run_once=true
