e6935617afd8c5c399a3b14035b5a32dcc9d111e
jsteeg <jan.n.steeg@student.fh-kiel.de>
2020-07-15 13:46:12 +0000

    changed paths

--- FasterRCNN/train.sh
+++ FasterRCNN/train.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-PIPELINE_CONFIG_PATH=./training/pipeline.config
-MODEL_DIR=./training
-NUM_TRAIN_STEPS=10
+PIPELINE_CONFIG_PATH=./model/faster_rcnn_inception_v2_ufo.config
 SAMPLE_1_OF_N_EVAL_EXAMPLES=1
-python3 ../../MASTERPROJECT/models/research/object_detection/model_main.py \
+python3 ~/models/research/object_detection/model_main.py \
     --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
     --model_dir=${MODEL_DIR} \
     --num_train_steps=${NUM_TRAIN_STEPS} \
