ee6aa19be09bf5dfce70f416da30126573668f62
Jan Steeg <jan.n.steeg@student.fh-kiel.de>
2020-12-02 17:33:39 +0100

    update

--- report_utils/plot_losses.py
+++ report_utils/plot_losses.py
@@ -10,13 +10,17 @@ LOSS = "Value"
 def main():
     # training data
     df = pandas.read_csv(train)
-    rolling = 20
-    df['Rolling'] = df.Value.rolling(rolling, 1).mean()
+    rolling = 101
+    df['Rolling'] = df.Value.rolling(rolling, 10).mean()
+    df['epoch'] = df.Value.rolling(rolling).mean()
+    df_epoch = df.iloc[::rolling, :]
+    print(df)
 
     x = df[STEP]
     y = df[LOSS]
-    # plt.plot(x, y, label="Train (raw)")
+    plt.plot(x, y, label="Train (raw)")
     plt.plot(x, df['Rolling'], label=f"Train (Rolling avg of {rolling})")
+    plt.plot(df_epoch[STEP], df_epoch['epoch'], label=f"Train (averaged)")
 
     # validation data
     df = pandas.read_csv(eval)
